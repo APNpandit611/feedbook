072ce0e4972fcecd86470a33387d0da9ef20d407
APNpandit611 <apnpanditt@gmail.com>
2024-11-19 14:08:04 +0200

    little touch on multer and cloudinary

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -10,7 +10,7 @@ export const userPost = async (req, res) => {
 
         if (req.file) {
             // Upload image to Cloudinary and get the URL
-            const cloudinaryResponse = await uploadImage(req.file.path.replace('./public', ''));
+            const cloudinaryResponse = await uploadImage(req.file.path);
             pictureUrl = cloudinaryResponse.secure_url;
         }
         const post = await UserPost.create({
