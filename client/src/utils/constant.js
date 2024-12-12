801572ac294cdc74fb87251c8b9ae9c5458b5d36
APNpandit611 <apnpanditt@gmail.com>
2024-12-12 18:00:02 +0200

    URL fixed

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user"
-export const USER_POST_API_END_POINT = "http://localhost:5000/api/v1/post"
+export const USER_API_END_POINT = "https://feedbook-48dd.onrender.com/api/v1/user"
+export const USER_POST_API_END_POINT = "https://feedbook-48dd.onrender.com/api/v1/post"
